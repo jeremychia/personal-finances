5613189e1c60d87c0c744ffca55f5b072b003d32
jeremychia <jeremyjchia@gmail.com>
2026-06-02 16:39:44 +0200

    feat(miles&more): add n26 transfers as code

--- dbt/models/staging/bank/gsheet/stg_bank_de_eur_milesmore_ftl_db.sql
+++ dbt/models/staging/bank/gsheet/stg_bank_de_eur_milesmore_ftl_db.sql
@@ -1,3 +1,5 @@
+-- Miles & More FTL DB staging with N26 transfer offsets
+-- Combines Miles & More vouchers with offsetting N26 transfers to balance clearing dates
 with
 source as (
     select *
@@ -17,7 +19,30 @@ renamed as (
         trim(reason_for_payment) as description
 
     from source
+),
+
+-- N26 transfers that funded Miles & More credit cards (reversed to offset)
+-- this is because the ledger from credit statements do not have transfers
+n26_transfers as (
+    select
+        'miles&more-ftl-db' as bank_source,
+        local_currency,
+        category,
+        local_date,
+        -local_amount as local_amount,
+        description
+    from {{ ref("stg_bank_de_eur_n26") }}
+    where category = 'Transfer'
+        and description like '%Deutsche Bank AG%'
+        and description like '%DE33500700100707099806%'
+        and description like '%5426520204005807%'
+),
+
+combined as (
+    select * from renamed
+    union all
+    select * from n26_transfers
 )
 
 select *
-from renamed
+from combined
